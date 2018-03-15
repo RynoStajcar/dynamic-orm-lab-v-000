c3a5884f6f790f2bf0c428c6056ed18e46702618
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-15 21:11:32 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,8 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    value = attribute.values.first.class == Fixnum ? value : "'#{attribute.values.first}'"
+    value = attribute.values.first
+    final_value = attribute.values.first.class == Fixnum ? value : "'#{value}'"
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = #{value}"
     DB[:conn].execute(sql)
   end
